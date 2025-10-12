aa72534f7ee209ce0eab9f3ed64896e8886dc90d
Rosa Turner <rmturner1220@gmail.com>
2025-10-12 11:47:18 -0400

    Login Page

--- scripts/api.js
+++ scripts/api.js
@@ -6,9 +6,11 @@
     host === "127.0.0.1" ||
     /^\d{1,3}(\.\d{1,3}){3}$/.test(host); // any LAN IP like 192.168.x.x
 
-  window.API = isLocal
-    ? `http://${host}:3000` // your local backend
-    : "https://sdev255-m06-tutorial-backend.onrender.com"; // your Render backend
+let api = isLocal
+    ? `http://${host}:3000`
+    : "https://sdev255-m06-tutorial-backend.onrender.com";
+
+ window.API = api.replace(/\/+$/, "");
 
   console.log("API base:", window.API);
 })();
